51ada3e99495d565a2e37d98d13b3e2a229d116f
Raghvendra Singh Bhadouriya <raghvendrabhadouriya2003@gmail.com>
2025-05-15 17:44:51 +0530

    again change cors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # raghvendra_bhadouriya_portfolio_bc
+
+"@types/cors": "^2.8.18"
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const express = require('express')
+const express = require("express")
 const PORT = process.env.PORT || 3000
 const server = express();
 const cors = require("cors")
@@ -8,10 +8,22 @@ const Connection = require("./config/db")
 const projectRouter = require("./Routes/projectsRoute")
 const skillRouter = require("./Routes/skillsRoute")
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://raghvendra-bhadouriya-portfolio.vercel.app'
+];
+
 server.use(cors({
-    origin: 'https://raghvendra-bhadouriya-portfolio.vercel.app/',
-    credentials: true
-  }))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}));
+
 server.use(express.json())
 server.use("/", projectRouter)
 server.use("/", skillRouter)
